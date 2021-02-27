e11a075094d8d66219472f4e3f1d8ca961ac12fd
bluesatyr <shawnevans.music@gmail.com>
2021-02-27 16:32:43 -0500

    save progress

--- server.js
+++ server.js
@@ -14,6 +14,31 @@ const connection = mysql.createConnection({
   
   connection.connect(err => {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
+    console.log('connected to company database as id ' + connection.threadId);
     afterConnection();
   });
+
+afterConnection = () => {
+    const sql = `SELECT employee.firstName, employee.lastName, role.title, role.salary, department.name, manager_id 
+    FROM employee LEFT JOIN role 
+    ON employee.role_id = role.id 
+    LEFT JOIN department 
+    ON role.department_id = department.id`;
+    const employeeObj = []; 
+    connection.query(sql, function(err, res) {
+        for (let i=0; i<res.length; i++) {
+          if (res[i].manager_id) {
+            connection.query(`SELECT firstName, lastName FROM employee WHERE id = ${res[i].manager_id}`, function(err, result) {
+              res[i].manager_name = `${result[0].firstName} ${result[0].lastName}`;
+              //console.log(res[i]);
+              employeeObj.push(res[i]);
+            });
+          }
+          else {
+            employeeObj.push(res[i]);
+          }
+        }
+      console.log(employeeObj);
+        connection.end;
+    });
+};
